6106ae6b4714f3d4522e729bf6ea3cef30afc97a
Gretzy96 <seangrenham3g@gmail.com>
2018-07-04 09:10:42 +0100

    YURT

---
+++ app/src/main/java/com/example/ezmilja/ericsson_internal_map/Constants.java
@@ -0,0 +1,6 @@
+package com.example.ezmilja.ericsson_internal_map;
+
+public class Constants {
+
+    public static final String SCAN_BAR_TEST_KEY= "barCodeText";
+}
--- app/src/main/java/com/example/ezmilja/ericsson_internal_map/GroundFloorFunctions.java
+++ app/src/main/java/com/example/ezmilja/ericsson_internal_map/GroundFloorFunctions.java
@@ -1,13 +1,81 @@
 package com.example.ezmilja.ericsson_internal_map;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class GroundFloorFunctions extends AppCompatActivity {
+public class GroundFloorFunctions extends Activity implements OnClickListener {
+
+    private PopupMenu popupMenu;
+    ImageButton img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ground_floor_functions);
+    }
+
+    @Override
+    public void onClick(View anchor) {
+        // TODO Auto-generated method stub
+
+        // Image switching when arrow is clicked
+        img= (ImageButton) findViewById(R.id.imageButton);
+        img.setImageResource(R.drawable.ic_baselinearrow_drop_down24px);
+
+        popupMenu = new PopupMenu(GroundFloorFunctions.this, anchor);
+        popupMenu.setOnDismissListener(new OnDismissListener());
+        popupMenu.setOnMenuItemClickListener(new OnMenuItemClickListener());
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    private class OnDismissListener implements PopupMenu.OnDismissListener {
+
+        @Override
+        public void onDismiss(PopupMenu menu) {
+            // TODO Auto-generated method stub
+            img.setImageResource(R.drawable.ic_baselinearrow_drop_up24px);
+
+        }
+
+    }
+
+    private class OnMenuItemClickListener implements
+            PopupMenu.OnMenuItemClickListener
+          {
+
+
+
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            // TODO Auto-generated method stub
+            switch (item.getItemId()) {
+                case R.id.lang_java:
+                    Toast.makeText(getApplicationContext(), "Java got clicked", Toast.LENGTH_SHORT).show();
+                    img.setImageResource(R.drawable.ic_baselinearrow_drop_up24px);
+                    return true;
+                case R.id.lang_android:
+                    Toast.makeText(getApplicationContext(), "Android got clicked",Toast.LENGTH_SHORT).show();
+                    img.setImageResource(R.drawable.ic_baselinearrow_drop_up24px);
+                    return true;
+                case R.id.lang_python:
+                    Toast.makeText(getApplicationContext(), "Python got clicked", Toast.LENGTH_SHORT).show();
+                    img.setImageResource(R.drawable.ic_baselinearrow_drop_up24px);
+                    return true;
+                case R.id.lang_ruby:
+                    Toast.makeText(getApplicationContext(), "Ruby got clicked", Toast.LENGTH_SHORT).show();
+                    img.setImageResource(R.drawable.ic_baselinearrow_drop_up24px);
+                    return true;
+
+            }
+            return false;
+        }
     }
 }
