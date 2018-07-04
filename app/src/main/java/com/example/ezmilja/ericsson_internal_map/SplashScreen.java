d86e1aeb228df671c7391cd51037232bbcbb6125
Gretzy96 <seangrenham3g@gmail.com>
2018-07-04 12:36:08 +0100

    YURT V2

--- app/src/main/java/com/example/ezmilja/ericsson_internal_map/SplashScreen.java
+++ app/src/main/java/com/example/ezmilja/ericsson_internal_map/SplashScreen.java
@@ -1,30 +1,90 @@
 package com.example.ezmilja.ericsson_internal_map;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        Thread myThread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    sleep(2000);
-                    Intent intent = new Intent(getApplicationContext(),MainMenu.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();
+        requestPermission();
+
+
 
     }
+    private void requestPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat
+                    .requestPermissions(SplashScreen.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // Permission Granted
+                    Toast.makeText(SplashScreen.this, "Permission Granted", Toast.LENGTH_SHORT)
+                            .show();
+
+                    Thread myThread = new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                sleep(2000);
+                                Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                                startActivity(intent);
+                                finish();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    myThread.start();
+
+                }
+
+                else
+                    {
+                    // Permission Denied
+                    Toast.makeText(SplashScreen.this, "Permission Denied", Toast.LENGTH_SHORT)
+                            .show();
+
+                        Thread myThread = new Thread() {
+                            @Override
+                            public void run() {
+                                try {
+                                    sleep(2000);
+                                    Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                                    startActivity(intent);
+                                    finish();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        myThread.start();
+
+                    }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
